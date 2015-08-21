1923a0d5fbf3870cff72110c2c78afde92666e77
Aaron <amaxpierce@yahoo.com>
2015-08-21 18:21:58 -0500

    daffd

--- routes/weather.js
+++ routes/weather.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:dir/:file', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
-  var dir = req.params.dir,
-      file = req.params.file;
 
   res.sendfile('./public/weather.html');
 
