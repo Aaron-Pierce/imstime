9cdc799b1aaf942c513cd5c653556e0eaa4dde91
Aaron <amaxpierce@yahoo.com>
2015-09-19 16:50:31 -0500

    added route for /music

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var weather = require('./routes/weather');
-
+var music = require('./routes/music');
 var app = express();
 
 // view engine setup
@@ -25,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/weather', weather)
+app.use('/weather', weather);
+app.use('/music', music);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
