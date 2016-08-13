79893a412e8cfe0b57b6ff4ee0df6165110dad78
Aaron <amaxpierce@yahoo.com>
2016-08-13 16:15:17 -0500

    Changed the timer sound, and even possibly maybe fixed it?

--- time.js
+++ time.js
@@ -13,40 +13,23 @@ var freshmanEndTimeWed = ["0840", "0855", "0948", "1021", "1114", "1204", "1255"
 var upperStartTimeWed = ["0730", "0835", "0900","1008", "1112", "1207", "1302", "1406", "1510"];
 var upperEndTimeWed = ["0820", "0924", "1016", "1110", "1204", "1258", "1352", "1446", "1540"];
 
-var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-var d = new Date();
-var dotw = days[d.getDay()];
-if(dotw === "wednesday"){
+var load = 0;
+var startTime;
+var endTime;
+var startTimeWed;
+var endTimeWed;
+
+var currentHours;
+var currentMinutes;
+var currentTime;
+initGrade();
+if(wednesday()){
     freshmanStartTime = freshmanStartTimeWed;
     freshmanEndTime = freshmanEndTimeWed;
     upperStartTime = upperStartTimeWed;
     upperEndTime =  upperEndTimeWed;
 }
 
-var currentHours = d.getHours();
-var currentMinutes = d.getMinutes();
-var currentTime = currentHours + "" + currentMinutes;
-console.log("initialised timejs");
-
-
-function reinitTime() {
-    d = new Date();
-    currentHours = d.getHours();
-    currentMinutes = d.getMinutes();
-    if(currentMinutes < 10){
-        currentMinutes = "0" + currentMinutes
-    }
-    if(currentHours < 10){
-        currentHours = "0" + currentHours
-    }
-
-    currentTime = currentHours + "" + currentMinutes;
-}
-
-
-var timeLeft;
-var hour;
-var inval = 0;
 var loadedGrade = localStorage.getItem("grade");
 var parser = document.createElement('a');
 parser.href = window.location.href;
@@ -57,6 +40,85 @@ if(loadedGrade === null){
     }else if(parser.hash + "" !== undefined){
         localStorage.setItem("grade", parser.hash.replace("#", ""));
     }
+}
+var endtime1;
+var startTimeNext;
+function main() {
+    for(var x = 0; x < startTime.length; x++){
+        endtime1 = endTime[x];
+        startTimeNext = startTime[x+1];
+        if(parseInt(currentTime) > startTime[x] && parseInt(currentTime) < endTime[x]){
+            var timeleft = (parseInt(endTime[x]) - parseInt(currentTime));
+            // console.log(currentHours[x] !== endTime[x].substring(0,2) + ", the hour ends during the same hour as the current one");
+            if(currentHours[x] !== endTime[x].substring(0,2)){
+                timeleft -= 40;
+            }
+            console.log("It is currently " + currentTime + ", the hour ends at " + endTime[x] + ". The hour ends in " + timeleft +  " minutes");
+            $(".minutesLeft").html(timeleft);
+            // console.log(parseInt(endTime[x]) - parseInt(currentTime) + "");
+        }else if(parseInt(currentTime) > endTime[x] && parseInt(currentTime) < startTime[x+1]){
+            var timeleft = parseInt(startTime[x+1]) - parseInt(currentTime);
+            if(currentHours[x] !== startTime[x+1].substring(0,2)){
+                timeleft -= 40;
+            }
+            console.log("It is currently passing period, the next hour begins at " +startTime[x+1]+ " and that is in " + timeleft + " minutes");
+            $(".time").html("There Are " + timeleft + " Minutes Left Until Next Hour");
+
+        }else{
+            console.log("No valid time found");
+            if(currentTime > endTime[endTime.length-1]){
+                $(".time").html("School is over")
+            }
+        }
+    }
+}
+
+
+
+function initGrade() {
+    var grade = localStorage.getItem("grade");
+    console.log("Grade " + grade + " Was Found");
+    if(grade === "freshman"){
+        console.log("Converting to freshman mode");
+        startTime = freshmanStartTime;
+        endTime = freshmanEndTime;
+        startTimeWed = freshmanStartTimeWed;
+        endTimeWed = freshmanEndTimeWed;
+    }else{
+        console.log("Converting to upperclassman mode");
+        startTime = upperStartTime;
+        endTime = upperEndTime;
+        startTimeWed = upperStartTimeWed;
+        endTimeWed = upperEndTimeWed;
+    }
+
+}
+
+function wednesday() {
+    var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    var d = new Date();
+    var dotw = days[d.getDay()];
+    return dotw === "wednesday";
+}
+
+function getTime() {
+    var d = new Date();
+    currentHours = d.getHours().toString();
+    if(currentHours.length === 1){
+        currentHours = " " + currentHours;
+    }
+    currentMinutes = d.getMinutes().toString();
+    if(currentMinutes.length === 1){
+        currentMinutes = " " + currentHours;
+    }
+    currentTime = currentHours + "" + currentMinutes;
+}
+//TODO: uncomment this for prod
+setInterval(function () {
+
+    getTime();
+    main();
+}, 500);
 }
 
 function main() {
